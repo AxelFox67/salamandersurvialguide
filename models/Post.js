3720b38286f00cfd7de14ac23caf976b32371f95
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-08 18:54:59 -0700

    updated pots to post

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Posts extends Model {}
+class Post extends Model {}
 
-Posts.init(
+Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -44,4 +44,4 @@ Posts.init(
   }
 );
 
-module.exports = Posts;
+module.exports = Post;
